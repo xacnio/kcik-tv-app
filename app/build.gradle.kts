33bcb30d08e115a8dd935d07c2060c6d274b5475
xacnio <me@xacnio.dev>
2026-06-05 16:36:50 +0300

    chore: bump version to 2.2.2-beta (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "dev.xacnio.kciktv"
         minSdk = 23
         targetSdk = 34
-        versionCode = 6
-        val baseVersion = "2.2.1-beta"
+        versionCode = 7
+        val baseVersion = "2.2.2-beta"
         versionName = "$baseVersion-${getGitHash()}"
     }
 
